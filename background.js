a66ad234930a9f4baaade2c48a8b1fe468f94231
King <wyk17@qq.com>
2021-11-29 15:33:08 +0800

    :sparkles: omnibox search

--- background.js
+++ background.js
@@ -5,3 +5,10 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+// This event is fired with the user accepts the input in the omnibox.
+chrome.omnibox.onInputEntered.addListener((text) => {
+  // Encode user input for special characters , / ? : @ & = + $ #
+  var newURL = 'https://kaifa.baidu.com/searchPage?wd=' + encodeURIComponent(text);
+  chrome.tabs.create({ url: newURL });
+});
