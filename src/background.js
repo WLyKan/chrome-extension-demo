c0a269a04bb456e22ab8e5604d9050acfb476958
King <wyk17@qq.com>
2021-12-01 15:30:32 +0800

    :recycle: Optimize the directory

--- src/background.js
+++ src/background.js
@@ -15,3 +15,15 @@ chrome.omnibox.onInputEntered.addListener((text) => {
 
 chrome.action.setBadgeText({text: 'ON'});
 chrome.action.setBadgeBackgroundColor({color: '#4688F1'});
+
+chrome.contextMenus.create({
+  id: 'contextMenusSearch',
+  title: '搜索：%s', // %s 表示选中的文字
+  contexts: ['selection'], // 只有当选中文字时才会出现此右键菜单
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'contextMenusSearch') {
+    chrome.tabs.create({url: 'https://kaifa.baidu.com/searchPage?wd=' + encodeURI(info.selectionText)});
+  }
+})
